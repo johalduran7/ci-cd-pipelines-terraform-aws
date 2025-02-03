7e64370118ab9de66e35b4f342c7b1301941eb3f
John Duran <johalduran@gmail.com>
2025-02-03 17:13:32 -0500

    no apply

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  region = "us-east-1" # Specify the region you want
-  #access_key = var.AWS_ACCESS_KEY_ID
-  #secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "us-east-1" 
 }
 
 # Create VPC
@@ -30,6 +28,7 @@ module "asg" {
 }
 
 data "aws_subnets" "available_subnets" {
+  
 }
 
 
