6e49e2db8882bfaa0302de2fe1684dee5d17cdec
John Duran <johalduran@gmail.com>
2025-02-03 16:02:10 -0500

    no apply

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region  = "us-west-2"  # Specify the region you want
+  #access_key = var.AWS_ACCESS_KEY_ID
+  #secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
 # Create VPC
 module "vpc" {
   source     = "./modules/vpc"
