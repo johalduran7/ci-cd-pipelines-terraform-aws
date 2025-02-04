0d312d0e8d366c92060f84e4f65da4e9e0902ab2
John Duran <johalduran@gmail.com>
2025-02-03 22:50:31 -0500

    adding ansible.yml

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -31,6 +31,3 @@ data "aws_subnets" "available_subnets" {
 
 }
 
-
-#since I had to add the ssh-key as an environment variable. to run this, you have to run: 
-#terraform apply  -auto-approve -var 'ssh_public_key=$TF_VAR_ssh_public_key'
