c477d16503ab218c7d005bd3495087f390c72eba
John Duran <johalduran@gmail.com>
2025-02-07 13:03:17 -0500

    release dev-infra-v1.0.6

--- app/terraform/modules/asg/ec2_permissions.tf
+++ app/terraform/modules/asg/ec2_permissions.tf
@@ -41,6 +41,27 @@ resource "aws_iam_policy" "cloudwatch_log_policy_agent" {
   })
 }
 
+
+# IAM Policy for SSM Access
+resource "aws_iam_role_policy" "ssm_read_policy" {
+  name = "ssm-read-policy"
+  role = aws_iam_role.ec2_app_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters"
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:ssm:us-east-1:948586925757:parameter/app/dev/app_version"
+      }
+    ]
+  })
+}
+
 # Attach IAM Policy to Role
 resource "aws_iam_role_policy_attachment" "attach_policy_cw_agent" {
   role       = aws_iam_role.ec2_execution_role.name
@@ -52,9 +73,9 @@ resource "aws_iam_role_policy_attachment" "attach_policy_ecr" {
   policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy_ssm" {
+resource "aws_iam_role_policy_attachment" "attach_policy_read_ssm" {
   role       = aws_iam_role.ec2_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+  policy_arn = aws_iam_policy.ssm_read_policy.arn
 }
 
 
