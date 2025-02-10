d591de6ebfa9b6ce9edb8c319770ce7e0ddf4a19
John Duran <johalduran@gmail.com>
2025-02-09 22:23:04 -0500

    cleaning repo

--- docker/p1_docker_aws/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "p1-docker-app-local-2" {
-  name                 = "p1-docker-app-local-2"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
